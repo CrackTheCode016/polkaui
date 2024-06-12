79f212191f0ad87838f88e1c93246423a30e8d1f
Bader Youssef <ibnbassem@gmail.com>
2024-06-12 14:48:35 -0400

    primevue stuff

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,19 @@ import Dropdown from 'primevue/dropdown';
 import Button from 'primevue/button';
 import Menubar from 'primevue/menubar';
 import { ref } from "vue";
+
+const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home'
+    },
+    {
+        label: 'Features',
+        icon: 'pi pi-star'
+    },
+]);
 </script>
 
 <template>
+    <Menubar :model="items" />
 </template>
