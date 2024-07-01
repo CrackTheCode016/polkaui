255b0005298f4aaaddbc5ddc1f121da50dbaf3c2
Bader Youssef <ibnbassem@gmail.com>
2024-07-01 17:26:21 -0400

    clean up

--- src/components/Upgrade.vue
+++ src/components/Upgrade.vue
@@ -10,11 +10,7 @@ import FileUpload from 'primevue/fileupload';
 const rpCurl = ref("wss://rpc.web3educhain.xyz:443");
 const typedApi = store.client.getTypedApi(roc);
 const call = typedApi.tx.System.remark({ remark: Binary.fromText("hi") });
-const authorizeExt = typedApi.tx.System.authorize_upgrade({ remark: Binary.fromText("hi") });
 
-function onUpload(event: any) {
-    console.log(event)
-}
 </script>
 
 <template>
